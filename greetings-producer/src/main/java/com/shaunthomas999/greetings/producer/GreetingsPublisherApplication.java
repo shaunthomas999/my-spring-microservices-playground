800426f811d7d7699f785dbe01b8da1885ebd3c5
Shaun Thomas <shaunthomas999@gmail.com>
2022-11-24 13:31:36 +0100

    Added greetings-producer module

---
+++ greetings-producer/src/main/java/com/shaunthomas999/greetings/producer/GreetingsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.shaunthomas999.greetings.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsPublisherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingsPublisherApplication.class, args);
+  }
+
+}
---
+++ greetings-producer/src/test/java/com/shaunthomas999/greetings/producer/GreetingsPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaunthomas999.greetings.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsPublisherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
