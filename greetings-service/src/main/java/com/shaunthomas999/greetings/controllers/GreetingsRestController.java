9d2a599d174acd89f2a24112268befea97406b56
Shaun Thomas <shaunthomas999@gmail.com>
2022-11-24 13:12:16 +0100

    refactored

--- greetings-service/src/main/java/com/shaunthomas999/greetings/controllers/GreetingsRestController.java
+++ greetings-service/src/main/java/com/shaunthomas999/greetings/controllers/GreetingsRestController.java
@@ -1,4 +1,4 @@
-package com.shaunthomas999.greetings;
+package com.shaunthomas999.greetings.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
